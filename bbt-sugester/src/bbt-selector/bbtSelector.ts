15da48ad5ee3d7ea97d8c842636a01af229ed1e7
FreezeSnail <smogonv3@gmail.com>
2020-10-24 14:34:30 -0400

    more main fixes

--- bbt-sugester/src/bbt-selector/bbtSelector.ts
+++ bbt-sugester/src/bbt-selector/bbtSelector.ts
@@ -1,8 +1,8 @@
 import { tempuratureIndex, weatherType } from "./bbtOptionEnums"
 
-const bases: string[] = ["oolong","white","black","green"],
-const flavors: string[] = ["acai berry","blueberry","Cherry","Grapefruit","Guava","Green apple","Honey lemon","Honeydew","Honey ginger","Kiwi","Lavender","Lychee","Lemon","Mulberry","Mango","Orange","Papaye","Passionfruit","Pina colada","Peach","Pineapple","Pomegranate","Rose","Raspberry","Strawberry","White peach"],
-const toppings: string[] = ["none", "aloe","coconut jelly","orea crumbs","red bean","pearls","popping pearls"]
+const bases: string[] = ["oolong","white","black","green"];
+const flavors: string[] = ["acai berry","blueberry","Cherry","Grapefruit","Guava","Green apple","Honey lemon","Honeydew","Honey ginger","Kiwi","Lavender","Lychee","Lemon","Mulberry","Mango","Orange","Papaye","Passionfruit","Pina colada","Peach","Pineapple","Pomegranate","Rose","Raspberry","Strawberry","White peach"];
+const toppings: string[] = ["none", "aloe","coconut jelly","orea crumbs","red bean","pearls","popping pearls"];
 
 interface Bbt {
     milk: boolean;
@@ -11,7 +11,7 @@ interface Bbt {
     topping: string;
 }
 
-function getRandomInt(max) {
+function getRandomInt(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
--- bbt-sugester/src/frontend/components/dropDownComp.jsx
+++ bbt-sugester/src/frontend/components/dropDownComp.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Select from 'react-select'
 import './components.sass';
 
 export class DropDownComp extends React.Component {
